74f8e19a09bc859f2ba61e44be779f9d1a1c7f33
Mauro Goyeneche <48460577+maurogoyeneche@users.noreply.github.com>
2022-01-27 12:39:37 -0300

    creo que eslint quedo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-
 import React from 'react';
-
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <label htmlFor="task">Task</label>
-      <input id="task" type="text" />
+  
     </div>
   )
 }

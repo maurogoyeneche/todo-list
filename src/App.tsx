04da2e647823e927bfe1237d92fcabe95e9c80cc
Mauro Goyeneche <48460577+maurogoyeneche@users.noreply.github.com>
2022-01-28 15:17:45 -0300

    sections todo-doing-done

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import InsertTask from 'components/InsertTask/InsertTask';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="mt-5 text-start mb-5">My To-Do List</h1>
-      <InsertTask/>
-      
-  
-    </div>
-  )
+	return (
+		<div className='App px-3'>
+			<div className='container text-start'>
+				<h1 className='mt-5  mb-5'>My To-Do List</h1>
+				<div className='row'>
+					<div className='col-md-4 border-end'>
+						<pre className='text-muted '>To do</pre>
+						<InsertTask />
+					</div>
+					<div className='col-md-4 border-end'>
+						<pre className='text-muted'>Doing</pre>
+					</div>
+					<div className='col-md-4'>
+						<pre className='text-muted'>Done</pre>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/InsertTask/InsertTask.tsx
+++ src/components/InsertTask/InsertTask.tsx
@@ -1,54 +1,57 @@
 import React, { useState } from 'react';
 
-interface Task { 
-    task: string;
-};
-
-
 const InsertTask = () => {
-  const [task, setTasks] = useState<Task[]>([]);
-  console.log(task)
-	const [inputValue, setInputValue] = useState('');
+	const [task, setTasks] = useState<Array<string>>([]);
+	const [value, setValue] = useState<string>('');
 
-	const addTask = (e:any) => {
-    e.preventDefault();
-    setTasks([...task, {task: inputValue}]);
-   
-	
+	const handleSubmit = (ev: any) => {
+		ev.preventDefault();
+		if (value) setTasks([...task, value]);
+		setValue('');
 	};
 
 	return (
-		<div>
-			<form action='' className='d-flex '>
-				<label htmlFor='task'>
-					<input
-						id='task'
-						className='form-control'
-						type='text'
-						name='task'
-            value={inputValue}
-						placeholder='Insert tasks here...'
-            onChange={(
-              ev: React.ChangeEvent<HTMLInputElement>,
-          ): void => setInputValue(ev.target.value)}
-					/>
-				</label>
-				<button
-					type='button'
-					className='btn btn-outline-primary'
-					onClick={(e) => addTask(e)}
-				>
-					Add
-				</button>
-        <p>{inputValue}</p>
-      
+		<div className=''>
+			<form action='' onSubmit={handleSubmit}>
+				<div className='d-flex  align-items-baseline w-100'>
+					<label htmlFor='task' className='w-100'>
+						<input
+							id='task'
+							className='form-control w-100'
+							type='text'
+							name='task'
+							value={value}
+							placeholder='Insert tasks here...'
+							onChange={(ev: React.ChangeEvent<HTMLInputElement>): void =>
+								setValue(ev.target.value)
+							}
+						/>
+					</label>
+					<button type='submit' className='btn btn-outline-primary mx-1'>
+						Add
+					</button>
+				</div>
+				<p className=''>
+					You will add this new task: <strong>{value}</strong>
+				</p>
 			</form>
 			<ul className='list-group'>
-				{task && task.map(task =><li key={Math.random()} className='list-group-item border-0'>{task.task}</li> )}
+				{task &&
+					task.map(task => (
+						<li
+							key={Math.random()}
+							className=' d-flex justify-content-between list-group-item p-0 border-0'
+						>
+							{task}
+							<button
+								type='button'
+								className='btn btn-sm btn-outline-success mx-1'
+							>
+								Move
+							</button>
+						</li>
+					))}
 			</ul>
-      	{/* <ul className='list-group'>
-        	<li  className='list-group-item border-0'>{task?.task}</li>
-			</ul> */}
 		</div>
 	);
 };
